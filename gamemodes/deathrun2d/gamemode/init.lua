e0daa0e8e0c6990a5e497b4d1db80013935f7f8c
Dmitry <iam@feelthelemon.ga>
2016-05-20 01:17:43 +0300

    fix

--- gamemodes/deathrun2d/gamemode/init.lua
+++ gamemodes/deathrun2d/gamemode/init.lua
@@ -93,7 +93,7 @@ function GM:PlayerDeath( victim )
 end
 
 hook.Add( "PlayerSay", "restart", function( ply, text, team )
-	if game.GetMap() == "dr2d_prison_break_v1"
+	if game.GetMap() == "dr2d_prison_break_v1" then
 	if ( string.sub( text, 1, 6 ) == "/repos" ) then
 		if ply:Team() == TEAM_KILLERS then
 			ply:SetPos(Vector(1189.562744, 740.533936, -86.191582))
